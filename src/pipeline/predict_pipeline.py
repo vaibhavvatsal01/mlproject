7617787314c106a33b2cf2e21396e07ef73a799d
vaibhav vatsal <vaibhavvatsalpandey01@gmail.com>
2025-12-20 10:53:26 +0530

    utils

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import pandas as pd
-from src.excepton import CustomException
+from src.exception import CustomException
 from src.utils import load_object
 class PredictPipeline:
     def __init__(self):
